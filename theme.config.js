444eacc546d0018c938a18442f818a25431d484c
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2022-03-02 14:57:12 +1300

    Updated theme config

--- theme.config.js
+++ theme.config.js
@@ -1,12 +1,12 @@
 export default {
   github: 'https://github.com/shuding/nextra',
   docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
-  titleSuffix: ' – Nextra',
+  titleSuffix: ' – Te Wāhi Learn',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">Learn</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Static Site Generator
+        a Te Wāhi resource
       </span>
     </>
   ),
@@ -16,15 +16,15 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
+      <meta name="description" content="Learn: a Te Wāhi resource" />
+      <meta name="og:description" content="Learn: a Te Wāhi resource" />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
+      <meta name="twitter:image" content="https://cdn.tewahi.me/og.png" />
+      <meta name="twitter:site:domain" content="learn.tewahi.com" />
+      <meta name="twitter:url" content="https://learn.tewahi.com" />
+      <meta name="og:title" content="Learn: a Te Wāhi resource" />
+      <meta name="og:image" content="https://cdn.tewahi.me/og.png" />
+      <meta name="apple-mobile-web-app-title" content="Te Wāhi Learn" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
@@ -62,6 +62,6 @@ export default {
   nextLinks: true,
   footer: true,
   footerEditLink: 'Edit this page on GitHub',
-  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>,
+  footerText: <>MIT {new Date().getFullYear()} © Te Wāhi Auaha .</>,
   unstable_faviconGlyph: '👋',
 }
